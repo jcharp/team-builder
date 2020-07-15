fd7d238aa9997bfea1c3100c7012eaa9b106635e
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-07-14 20:25:35 -0400

    got name and submit button showing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
 function App() {
+  const [members, setMembers] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <form>
+        <label>Full name
+
+      
+        <input
+          maxLength='30'
+          placeholder='name'
+          id='nameInput'
+          name='name'
+          type="text"
+          />
+            </label>
+
+        <br/>
+        <input type="submit"/>
+        
+      </form>
+
+      
+
+
+
+
+
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href=""
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
