6aa5288bf454b3675a8143f5bc371fe60fc5f94b
unknown <jackcharp69@gmail.com>
2020-09-08 19:44:53 -0400

    started form

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,2 +1,37 @@
 import React from 'react';
 import './App.css';
+
+
+function Form() {
+
+return(
+  <form>
+      <label>
+          Name
+          <imput/>
+      </label>
+
+      <label>
+          Email
+          <input />
+      </label>
+
+      <label>
+          Rone
+          <select>
+              <option>Backend Developer</option>
+              <option>Frontedn Developer</option>
+              <option>Designer</option>
+              <option>Food Service</option>
+          </select>
+      </label>
+
+
+  </form>
+
+
+)
+
+}
+
+export default Form;
